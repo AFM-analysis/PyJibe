d80b13a3d008f9718b7394833d042b3fb322bb37
Paul Müller <dev@craban.de>
2020-01-10 16:57:51 +0100

    enh: show ancillary parameters in info tab

--- pyjibe/fd/tab_info.py
+++ pyjibe/fd/tab_info.py
@@ -2,6 +2,7 @@ import numbers
 import pkg_resources
 
 from afmformats import meta
+from nanite import model
 from PyQt5 import uic, QtWidgets
 
 
@@ -16,6 +17,7 @@ class TabInfo(QtWidgets.QWidget):
         text = []
         keys = list(fdist.metadata.keys())
 
+        # Dataset
         text.append("<b>Dataset</b>")
         data_keys = ["path", "enum"]
         for kk in data_keys:
@@ -24,6 +26,7 @@ class TabInfo(QtWidgets.QWidget):
             if kk in meta.DEF_DATA and kk not in data_keys:
                 text.append(get_string_rep(kk, fdist.metadata))
 
+        # Experiment
         if set(keys) & set(meta.DEF_EXPERIMENT.keys()):
             text.append("")
             text.append("<b>Experiment:</b>")
@@ -33,6 +36,7 @@ class TabInfo(QtWidgets.QWidget):
                     text_meta.append(get_string_rep(kk, fdist.metadata))
             text += sorted(text_meta)
 
+        # QMap
         if set(keys) & set(meta.DEF_QMAP.keys()):
             text.append("")
             text.append("<b>QMap:</b>")
@@ -42,6 +46,7 @@ class TabInfo(QtWidgets.QWidget):
                     text_meta.append(get_string_rep(kk, fdist.metadata))
             text += sorted(text_meta)
 
+        # Analysis
         if set(keys) & set(meta.DEF_ANALYSIS.keys()):
             text.append("")
             text.append("<b>Analysis:</b>")
@@ -49,6 +54,21 @@ class TabInfo(QtWidgets.QWidget):
             for kk in keys:
                 if kk in meta.DEF_ANALYSIS:
                     text_meta.append(get_string_rep(kk, fdist.metadata))
+            text += sorted(text_meta)
+
+        # Ancillaries
+        anc_dict = fdist.get_ancillary_parameters()
+        if anc_dict:
+            text.append("")
+            text.append("<b>Ancillaries:</b>")
+            text_meta = []
+            md = model.models_available[fdist.fit_properties["model_key"]]
+            for kk in anc_dict:
+                idk = md.parameter_anc_keys.index(kk)
+                text_meta.append("{}: {:.5g} {}".format(
+                    md.parameter_anc_names[idk],
+                    anc_dict[kk],
+                    md.parameter_anc_units[idk]))
             text += sorted(text_meta)
 
         textstring = "<br>".join(text)
