8fd31b552369357c90ee5938c8781914d426546e
Paul Müller <dev@craban.de>
2020-07-06 20:06:23 +0200

    ref: cleanup settings

--- pyjibe/settings.py
+++ pyjibe/settings.py
@@ -1,4 +1,5 @@
 """Settings file management"""
+import numbers
 import pathlib
 
 import appdirs
@@ -27,6 +28,73 @@ class SettingsFile(object):
         self.defaults = defaults
         self.working_directories = {}
 
+    def _get_value(self, dtype, key):
+        key = key.lower()
+        cdict = self.load()
+        if key in cdict:
+            val = cdict[key]
+        elif dtype in self.defaults and key in self.defaults[dtype]:
+            val = self.defaults[dtype][key]
+        else:
+            raise KeyError(
+                "Config key '{}' of type '{}' not set!".format(key, dtype))
+
+        if dtype == "bool":
+            if isinstance(val, bool):
+                pass
+            else:
+                if val.lower() not in ["true", "false"]:
+                    raise ValueError(
+                        "Config key '{}' not boolean!".format(key))
+                elif val.lower() == "true":
+                    val = True
+                else:
+                    val = False
+        elif dtype == "integer":
+            if isinstance(val, numbers.Integral):
+                pass
+            else:
+                if not val.isdigit():
+                    raise ValueError(
+                        "Config key '{}' is no integer!".format(key))
+                val = int(val)
+        elif dtype == "string_list":
+            if isinstance(val, list):
+                pass
+            else:
+                val = val.split("\t")
+
+        return val
+
+    def get_bool(self, key):
+        """Returns boolean configuration key"""
+        return self._get_value("bool", key)
+
+    def get_int(self, key):
+        """Returns integer configuration key"""
+        return self._get_value("integer", key)
+
+    def get_path(self, name=""):
+        """Returns the path for label `name`"""
+        wdkey = "path {}".format(name.lower())
+        try:
+            path = self._get_value("path", wdkey)
+        except KeyError:
+            path = "."
+        return path
+
+    def get_string(self, key):
+        """Return string"""
+        try:
+            val = self._get_value("string", key)
+        except KeyError:
+            val = ""
+        return val
+
+    def get_string_list(self, key):
+        """Returns list of strings"""
+        self._get_value("string_list", key)
+
     def load(self):
         """Loads the settings file returning a dictionary"""
         with self.cfgfile.open() as fop:
@@ -37,47 +105,6 @@ class SettingsFile(object):
             var, val = line.split("=", 1)
             cdict[var.lower().strip()] = val.strip()
         return cdict
-
-    def get_bool(self, key):
-        """Returns boolean configuration key"""
-        key = key.lower()
-        cdict = self.load()
-        if key in cdict:
-            val = cdict[key]
-            if val.lower() not in ["true", "false"]:
-                raise ValueError("Config key '{}' not boolean!".format(key))
-            if val == "True":
-                ret = True
-            else:
-                ret = False
-        else:
-            ret = self.defaults[key]
-        return ret
-
-    def get_int(self, key):
-        """Returns integer configuration key"""
-        key = key.lower()
-        cdict = self.load()
-        if key in cdict:
-            val = cdict[key]
-            if not val.isdigit():
-                raise ValueError("Config key '{}' is no integer!".format(key))
-            ret = int(val)
-        else:
-            raise KeyError("Config key `{}` not set!".format(key))
-        return ret
-
-    def get_path(self, name=""):
-        """Returns the path for label `name`"""
-        cdict = self.load()
-        wdkey = "path {}".format(name.lower())
-
-        if wdkey in cdict:
-            wd = cdict[wdkey]
-        else:
-            wd = "./"
-
-        return wd
 
     def save(self, cdict):
         """Save a settings dictionary into a file"""
@@ -109,6 +136,24 @@ class SettingsFile(object):
         cdict = self.load()
         wdkey = "path {}".format(name.lower())
         cdict[wdkey] = wd
+        self.save(cdict)
+
+    def set_string(self, key, value=""):
+        """Set the path in the settings file"""
+        if value.strip():
+            cdict = self.load()
+            cdict[key] = value
+            self.save(cdict)
+
+    def set_string_list(self, key, value):
+        """Set a list of strings"""
+        if not isinstance(value, list):
+            raise ValueError("`value` must be a list!")
+        for ch in ["\t", "\n"]:
+            if "".join(value).count(ch):
+                raise ValueError("List items must not contain {}!".format(ch))
+        cdict = self.load()
+        cdict[key.lower()] = "\t".join(value)
         self.save(cdict)
 
 
