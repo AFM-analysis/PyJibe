182f9b9faefc2baf913acda84cd474478c146ec8
Paul Müller <dev@craban.de>
2020-11-16 17:29:20 +0100

    setup: bump afmformats to 0.12.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     license="GPL v3",
     description=description,
     long_description=open('README.rst').read() if exists('README.rst') else '',
-    install_requires=["afmformats>=0.11.0",
+    install_requires=["afmformats>=0.12.0",
                       "appdirs",
                       "nanite>=1.6.2",
                       "matplotlib>=3",  # NavigationToolbar2QT mod
