eaf78a969307a7fcdc3bf3e5b50ebe7e6b81d932
Paul Müller <dev@craban.de>
2021-07-09 10:23:47 +0200

    setup: bump afmformats and nanite

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ setup(
     license="GPL v3",
     description=description,
     long_description=open('README.rst').read() if exists('README.rst') else '',
-    install_requires=["afmformats>=0.14.3",
-                      "nanite>=1.7.6",
+    install_requires=["afmformats>=0.15.0",
+                      "nanite>=1.7.8",
                       "matplotlib>=3",  # NavigationToolbar2QT mod
                       "pyqt5"],
     python_requires='>=3.6, <4',
