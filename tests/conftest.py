7b42d76f24efee407546535fea38073baa71072c
Paul Müller <dev@craban.de>
2022-01-06 16:51:56 +0100

    tests: added missing commands in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import shutil
 import tempfile
 import time
 
+from PyQt5 import QtCore
+
 TMPDIR = tempfile.mkdtemp(prefix=time.strftime(
     "pyjibe_test_%H.%M_"))
 
@@ -12,6 +14,16 @@ def pytest_configure(config):
     This hook is called for every plugin and initial conftest
     file after command line options have been parsed.
     """
+    # disable update checking
+    QtCore.QCoreApplication.setOrganizationName("AFM-Analysis")
+    QtCore.QCoreApplication.setOrganizationDomain("pyjibe.mpl.mpg.de")
+    QtCore.QCoreApplication.setApplicationName("PyJibe")
+    QtCore.QSettings.setDefaultFormat(QtCore.QSettings.IniFormat)
+    settings = QtCore.QSettings()
+    settings.setIniCodec("utf-8")
+    settings.setValue("check for updates", 0)
+    settings.sync()
+    # set global temp directory
     tempfile.tempdir = TMPDIR
 
 
@@ -19,4 +31,11 @@ def pytest_unconfigure(config):
     """
     called before test process is exited.
     """
+    QtCore.QCoreApplication.setOrganizationName("AFM-Analysis")
+    QtCore.QCoreApplication.setOrganizationDomain("pyjibe.mpl.mpg.de")
+    QtCore.QCoreApplication.setApplicationName("PyJibe")
+    QtCore.QSettings.setDefaultFormat(QtCore.QSettings.IniFormat)
+    settings = QtCore.QSettings()
+    settings.setIniCodec("utf-8")
+    # clear global temp directory
     shutil.rmtree(TMPDIR, ignore_errors=True)
--- tests/test_head_update.py
+++ tests/test_head_update.py
@@ -1,3 +1,4 @@
+import encodings.idna  # noqa: F401
 import os
 import socket
 
@@ -21,7 +22,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 @pytest.mark.skipif(not NET_AVAILABLE, reason="No network connection!")
 def test_update_basic():
     mdict = update.check_release(ghrepo="AFM-analysis/PyJibe",
-                                 version="0.11.0")
+                                 version="0.1.0")
     assert mdict["errors"] is None
     assert mdict["update available"]
     mdict = update.check_release(ghrepo="AFM-analysis/PyJibe",
